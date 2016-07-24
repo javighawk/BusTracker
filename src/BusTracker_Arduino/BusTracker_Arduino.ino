85c620d01bf9b58ba302f856412ebfbf2467cc1a
javighawk <jgarci42@hawk.iit.edu>
2016-07-24 15:38:35 -0600

    Added pseudo-interrupts for bus stop change and bus direction change (from/to downtown)

--- src/BusTracker_Arduino/BusTracker_Arduino.ino
+++ src/BusTracker_Arduino/BusTracker_Arduino.ino
@@ -5,24 +5,24 @@
 
 /* Defines */
 #define INT0_PIN              2           // Interrupt pin
+#define INT1_PIN              4           // Fake interrupt pin
+#define DIRECTION_PIN         6           // This pin determines if we are showing bus stops TO or FROM downtown.
+#define GREENLED_PIN          8           // Pin for red led (showing direction = FROM downtown)
+#define REDLED_PIN            9           // Pin for green led (showing direction = TO downtown)
 #define INT_PERIOD_MS         200         // Minimum time between interrupts in milliseconds (used to avoid interrupt bouncing)
 #define AveM_22nd_DT_ID       "3968"      // Bus Stop ID (Ave M & 22nd St, headed to downtown)
 #define AveM_22nd_FDT_ID      "3017"      // Bus Stop ID (Ave M & 22nd St, headed to confed. mall)
 #define PERIOD_MS             10000       // Period between website reading
 #define WATCHDOG_PERIOD       WDTO_8S     // Watchdog reset period (8 seconds)
 
-/*
- * Declare functions
- */
+/* Declare functions */
 void MAIN_retrieveBusStopInformation(BusStop *bs);
 extern int INET_getBusStopWebsite(char *busStopID);
 extern int INET_init();
 
-
 /* Bus stop objects */
 BusStop AveM_22nd_DT(AveM_22nd_DT_ID);
 BusStop AveM_22nd_FDT(AveM_22nd_FDT_ID);
-
 
 /* Current time & date extracted from website */
 int8_t currentHour, currentMin;
@@ -42,27 +42,26 @@ unsigned long lastTime = 0;
 /* Variable to avoud interrupt bouncing */
 unsigned long lastTimeInterrupt = 0;
 
+/* Time since colon was last changed */
+unsigned long lastTimeColon = 0;
+
 /* Array of BusStop pointers cointaining pointers to all BusStops TO Downtown */
-BusStop *allStops_DT[2] = {&AveM_22nd_DT};
+BusStop *allStops_DT[1] = {&AveM_22nd_DT};
 
 /* Array of BusStop pointers cointaining pointers to all BusStops FROM Downtown */
-BusStop *allStops_FDT[2] = {&AveM_22nd_FDT};
+BusStop *allStops_FDT[1] = {&AveM_22nd_FDT};
 
 /* Number of all stops TO Downtown */
 int nAllStops_DT = sizeof(allStops_DT)/sizeof(allStops_DT[0]);
 
 /* Number of all stops FROM Downtown */
-int nAllStops_FDT = sizeof(allStops_DT)/sizeof(allStops_DT[0]);
-
-/* Time since colon was last changed */
-long lastTimeColon = 0;
+int nAllStops_FDT = sizeof(allStops_FDT)/sizeof(allStops_FDT[0]);
 
 
 /*
  * Setup function
  */
 void setup(){
-
     // Initialize displayed bus stop and wait time indexes
     currentBusStop = 0;
     currentWaitTime = 0;
@@ -76,29 +75,58 @@ void setup(){
     // Setup interrupts
     attachInterrupt(digitalPinToInterrupt(INT0_PIN), int0_handler, RISING);
 
+    // Setup output pins
+    pinMode(REDLED_PIN, OUTPUT);
+    pinMode(GREENLED_PIN, OUTPUT);
+
+    // Set LEDs to initial direction
+    digitalWrite(REDLED_PIN, !currentDirectionDT);
+    digitalWrite(GREENLED_PIN, currentDirectionDT);
+
     // Enable watchdog
     wdt_enable(WATCHDOG_PERIOD);
 }
 
+
 /*
- * Main Loop. Each bus stop information is retrieve one after another
+ * Loop function.
  */
 void loop(){
-    // Retrieve updated bus stop information each PERIOD_MS milliseconds
+    // Update the direction of interest (TO/FROM downtown)
+    if( boolean(digitalRead(DIRECTION_PIN)) != currentDirectionDT ){
+        // Shift current direction
+        currentDirectionDT = !currentDirectionDT;
+      
+        // If direction has changed, force to download the new displayed bus stop
+        lastTime = 0;
+
+        // Set LEDs
+        digitalWrite(REDLED_PIN, !currentDirectionDT);
+        digitalWrite(GREENLED_PIN, currentDirectionDT);
+    }
+
+    // Trigger our fake interrupt if INT1_PIN is high
+    if( digitalRead(INT1_PIN) )
+        int1_handler();
+    
+    // Retrieve updated bus stop information each PERIOD_MS milliseconds.
+    // Only the currently shown bus stop is updated
     if( millis() - lastTime > PERIOD_MS ){
-        for( int i=0 ; i<nAllStops_DT ; i++ ) 
-            MAIN_retrieveBusStopInformation(allStops_DT[i]);
+        MAIN_retrieveBusStopInformation();
         lastTime = millis();
     }
+    
+    // Show user-selected bus stop in the display
+    if( currentDirectionDT )
+        DISP_showWaitTime(allStops_DT[currentBusStop], currentWaitTime);
+    else
+        DISP_showWaitTime(allStops_FDT[currentBusStop], currentWaitTime);
 
     // Switch colon
     if( millis() - lastTimeColon > 500 ){
         DISP_switchColon();
         lastTimeColon = millis();
     }
-    
-    // Show user-selected bus stop in the display
-    DISP_showWaitTime(allStops_DT[currentBusStop], currentWaitTime);
 
     // Feed watchdog timer
     wdt_reset();
@@ -107,11 +135,15 @@ void loop(){
 
 /*
  * Retrieves information from the website, stores it and displays it
- * 
- * @param bs1 BusStop object where the info will be stored. If this is null, then all BusStop objects will be considered
- * @param bs2 Optional additional BusStop if more than one line's info is wanted
  */
-void MAIN_retrieveBusStopInformation(BusStop *bs){
+void MAIN_retrieveBusStopInformation(){
+    // Get the bus stop of interest
+    BusStop *bs;
+    if( currentDirectionDT )
+        bs = allStops_DT[currentBusStop];
+    else
+        bs = allStops_FDT[currentBusStop];
+    
     // Reset wait times
     bs->resetTimes();
 
@@ -133,11 +165,34 @@ void int0_handler(){
     if( millis() - lastTimeInterrupt <= INT_PERIOD_MS )
         return;
 
-    // Update bus stop index
+    // Update waiting time index
     currentWaitTime = (currentWaitTime + 1) % WAITTIMES_N; 
 
     // Update last time interrupt was executed
     lastTimeInterrupt = nowTime;
+}
+
+
+/*
+ * Handler for a fake interrupt
+ * Changes the bus stop displayed
+ */
+void int1_handler(){
+    // Check time from last interrupt
+    unsigned long nowTime = millis();
+    if( millis() - lastTimeInterrupt <= INT_PERIOD_MS )
+        return;
+
+    // Update bus stop index
+    uint8_t nStops = (nAllStops_DT * currentDirectionDT) + (nAllStops_FDT * !currentDirectionDT);
+    currentBusStop = (currentBusStop + 1) % nStops;
+    if( nStops > 1 ){
+        // Force to download new bus stop information (if there's more than 1 bus stop)
+        lastTime = 0;
+    }
+    
+    // Update last time interrupt was executed
+    lastTimeInterrupt = nowTime;    
 }
 
 
