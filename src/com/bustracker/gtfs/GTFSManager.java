b9f88e51bdff3978c9b46d5c32db01eb8ba03838
Javier Garcia Gonzalez <jag053@mail.usask.ca>
2017-04-02 17:21:17 -0600

    Added implementation of GTFSManager

--- src/com/bustracker/gtfs/GTFSManager.java
+++ src/com/bustracker/gtfs/GTFSManager.java
@@ -5,10 +5,13 @@ import java.util.Set;
 import com.bustracker.bus.TripStop;
 
 import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
 
 public interface GTFSManager {
 	
-	Set<TripStop> getUpcomingTripsFromBusStop( String busStopId, int numberOfTrips );
+	Subscription subscribeToBusStopUpdates( 
+			String busStopId,  Action1<? super Set<TripStop>> action );
 	
-	Observable<Void> getNewUpdatesEvents();
+	void unsubscribeToBusStopUpdates( String busStopId );
 }
