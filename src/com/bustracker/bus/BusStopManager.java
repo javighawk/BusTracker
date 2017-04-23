c5c6e5f6940fe118d6e670cbe50f84d65cec0f72
Javier Garcia Gonzalez <jag053@mail.usask.ca>
2017-04-22 20:43:20 -0600

    Added function to unsubscribe bus stops

--- src/com/bustracker/bus/BusStopManager.java
+++ src/com/bustracker/bus/BusStopManager.java
@@ -1,8 +1,51 @@
 package com.bustracker.bus;
 
-import rx.Observable;
+import com.bustracker.gtfs.GTFSManager;
+import com.google.common.collect.Sets;
 
-public interface BusStopManager {
+import java.util.Optional;
+import java.util.Set;
 
-	Observable<TripStop> getTripStopUpdatedEvents();
+public class BusStopManager {
+	
+	private final GTFSManager gtfsManager;
+	private Set<BusStop> busStops = Sets.newHashSet();
+	
+	public BusStopManager( GTFSManager gtfsManager ) {
+		this.gtfsManager = gtfsManager;
+	}
+
+	public void addBusStopToTrack( String busStopName ) {
+		gtfsManager.getStaticData().getBusStopIDs( busStopName ).forEach( bs -> {
+			Set<TripStop> trips =
+					gtfsManager
+							.getStaticData()
+							.getAllScheduledTripStopsFromBusStopId(
+									bs );
+			busStops.add( new BusStop( bs, trips ) );
+			gtfsManager.subscribeToBusStopUpdates(
+					bs,
+					tss -> onTripStopsUpdates( bs, tss ) );
+		} );
+	}
+
+	public void removeBusStopToTrack( String busStopName ) {
+		gtfsManager.getStaticData().getBusStopIDs( busStopName )
+				.forEach( gtfsManager::unsubscribeToBusStopUpdates );
+	}
+
+	private void onTripStopsUpdates( 
+			String busStopId, Set<TripStop> tss ) {
+		getBusStop( busStopId )
+		.ifPresent( bs -> bs.setNewUpdates( tss ) );
+	}
+
+	public Optional<BusStop> getBusStop( String busStopId ) {
+		for( BusStop busStop : busStops ) {
+			if( busStop.getBusStopId().equals( busStopId ) ) {
+				return Optional.of( busStop );
+			}
+		}
+		return Optional.empty();
+	}
 }
