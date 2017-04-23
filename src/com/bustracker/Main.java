8cdcee7a5d2f7588394797ac41e1c09c9b40eee1
Javier Garcia Gonzalez <jag053@mail.usask.ca>
2017-04-22 21:16:23 -0600

    Modified main for an initial test

--- src/com/bustracker/Main.java
+++ src/com/bustracker/Main.java
@@ -1,87 +1,62 @@
 package com.bustracker;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import com.bustracker.bus.BusStopManager;
+import com.bustracker.gtfs.GTFSManager;
+import com.bustracker.gtfs.GTFSStaticData;
 
-import com.bustracker.bus.TripStopImpl;
-import com.bustracker.bus.BusStopImplBackup;
-import com.bustracker.gtfs.GTFSStaticDataImpl;
-import com.bustracker.userio.Display;
-import com.bustracker.userio.ButtonsManagerImpl;
-import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 	
 	/* Initialize user variables */
-	public static String gtfsPath = "gtfs/";
-	public static String gtfsURL = "http://apps2.saskatoon.ca/app/data/TripUpdate/TripUpdates.pb";
-	public static String[] bStop_names = {"22nd Street / Avenue M"};
+	private static String gtfsPath = "gtfs/";
+	private static String gtfsURL = "http://apps2.saskatoon.ca/app/data/TripUpdate/TripUpdates.pb";
+	private static ScheduledExecutorService executorService;
 	
-	/* Global variables */
-	public static List<BusStopImplBackup> bStops;
-	public static Comparator<TripStopImpl> waitTimeComp = new Comparator<TripStopImpl>(){
-		@Override
-		public int compare(TripStopImpl o1, TripStopImpl o2) {
-			return o1.getSchedTime().plusSeconds(o1.getDelay()).compareTo(o2.getSchedTime().plusSeconds(o2.getDelay()));
-		}		
-	};
-	
-	/* Shared variables */
-	public static GTFSStaticDataImpl gtfsdata = new GTFSStaticDataImpl();
-	public static Display disp;
-	public static URL url;
-	public static ButtonsManagerImpl gpio;
-	
-	
-	/*
-	 * Initialize bus tracker 
-	 * @param gtfsPath Path to the static GTFS files
-	 */
-	public static void initialize(String gtfsPath){
-		// Initialize display
-		try {
-			disp = new Display();
-		} catch (IOException | UnsupportedBusNumberException e) {
-			e.printStackTrace();
-		}
-				
-		// Load GTFS data
-		gtfsdata.parseFromPath(gtfsPath);
-		
-		// Initialize ArrayList with all the bus stops
-		bStops = new ArrayList<BusStopImplBackup>();
-		
-		// Initialize bus stop objects and add them to the set
-		for (String name : bStop_names)
-			bStops.add(new BusStopImplBackup(name));
-		
-		// Initialize URL
-		try {
-			url = new URL(gtfsURL);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
-		gpio = new ButtonsManagerImpl();		
-	}
-	
-	/*
-	 * Main function
-	 * @param args Arguments. Path to the GTFS static data
-	 */
-	public static void main(String[] args){
+	private static GTFSStaticData gtfsStaticData;
+	private static GTFSManager gtfsManager;
+	private static BusStopManager busStopManager;
 
-		// Initialize system
-		initialize(args[0]);
-		
-		// Start threads
-		for(BusStopImplBackup bstt : bStops)
-			bstt.start();
-		
-		disp.startDisplayBusStops();
+	public static void main( String[] args ){
+		executorService = getNewThreadPool( );
+		gtfsStaticData = new GTFSStaticData( gtfsPath );
+		gtfsManager = new GTFSManager(
+				gtfsURL,
+				Duration.ofSeconds( 10 ),
+				gtfsStaticData,
+				executorService );
+		busStopManager = new BusStopManager( gtfsManager );
+		setUpTest( );
+	}
+
+	private static void setUpTest( ) {
+		String busStopName = "Bryans / 108th Street";
+		Set<String> busStopIds = gtfsStaticData.getBusStopIDs( busStopName );
+		busStopManager.addBusStopToTrack(
+				busStopName );
+		busStopIds.forEach(
+				bsid -> executorService.schedule(
+						() -> busStopManager.getBusStop( bsid ).ifPresent(
+								bstop -> {
+										System.out.println(
+												busStopName );
+										System.out.println(
+												bstop.getBusStopId( ) );
+										System.out.println(
+												bstop.getUpcomingBus( 0 ) );
+										System.out.println(
+												bstop.getUpcomingBus( 1 ) );
+										System.out.println(
+												bstop.getUpcomingBus( 2 ) );
+								} ),
+						10, TimeUnit.SECONDS ) );
+	}
+
+	private static ScheduledThreadPoolExecutor getNewThreadPool( ) {
+		return new ScheduledThreadPoolExecutor( 10 );
 	}
 }
