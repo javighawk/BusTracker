bb29cb93c0a808fd23399eea82aee7b02781389c
Javier Garcia Gonzalez <jag053@mail.usask.ca>
2017-05-06 11:42:26 -0600

    Fixed import style

--- src/main/com/bustracker/trip/thread/TripStopThreads.java
+++ src/main/com/bustracker/trip/thread/TripStopThreads.java
@@ -1,6 +1,10 @@
 package com.bustracker.trip.thread;
 
-import java.util.concurrent.*;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Javier on 2017-04-29.
@@ -12,7 +16,7 @@ public final class TripStopThreads {
 
     public static ScheduledFuture<?> schedule( Runnable command ) {
         return INSTANCE.schedule(
-                command,1, TimeUnit.MINUTES );
+                command, 1, TimeUnit.MINUTES );
     }
 
     private TripStopThreads() {
