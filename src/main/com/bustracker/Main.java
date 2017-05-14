18c0e83924c3c09961c5b8dc1e96f8e5f8e625bb
Javier Garcia Gonzalez <jag053@mail.usask.ca>
2017-05-14 13:44:49 -0600

    Finalize dependency injection on Main

--- src/main/com/bustracker/Main.java
+++ src/main/com/bustracker/Main.java
@@ -3,76 +3,51 @@ package com.bustracker;
 import com.bustracker.bus.BusStopManager;
 import com.bustracker.gtfs.GTFSManager;
 import com.bustracker.gtfs.GTFSStaticData;
-import com.bustracker.userio.display.DisplayManager;
-import com.pi4j.io.i2c.I2CFactory;
+import com.bustracker.userio.UserIOManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.time.Duration;
-import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
-	
-	/* Initialize user variables */
-	private static String gtfsPath = "gtfs/";
-	private static String gtfsURL = "http://apps2.saskatoon.ca/app/data/TripUpdate/TripUpdates.pb";
-	private static ScheduledExecutorService executorService;
+    private static String gtfsPath = "gtfs/";
+    private static String gtfsURL =
+            "http://apps2.saskatoon.ca/app/data/TripUpdate/TripUpdates.pb";
+    private static Duration gtfsManagerTaskPeriod = Duration.ofSeconds( 10 );
+    private static Duration userIoTaskPeriod = Duration.ofSeconds( 10 );
+    private static int numberOfTripsToShow = 3;
 
-	private static GTFSStaticData gtfsStaticData;
-	private static GTFSManager gtfsManager;
-	private static BusStopManager busStopManager;
-	private static DisplayManager displayManager;
+    private static ScheduledExecutorService executorService;
+    private static GTFSStaticData gtfsStaticData;
+    private static GTFSManager gtfsManager;
+    private static BusStopManager busStopManager;
+    private static UserIOManager userIoManager;
     private static final Logger LOG = LoggerFactory.getLogger( Main.class );
 
-	public static void main( String[] args ){
-		executorService = getNewThreadPool( );
-		gtfsStaticData = new GTFSStaticData( gtfsPath );
-		gtfsManager = new GTFSManager(
-				gtfsURL,
-				Duration.ofSeconds( 10 ),
-				gtfsStaticData,
-				executorService );
-		busStopManager = new BusStopManager( gtfsManager );
-        try {
-            displayManager = new DisplayManager(
-                    busStopManager,
-                    Duration.ofSeconds( 10 ),
-                    3, executorService );
-        } catch( IOException | I2CFactory.UnsupportedBusNumberException e ) {
-            LOG.error( "Error initializing display", e );
-        }
-        setUpTest( );
-	}
+    public static void main( String[] args ){
+        LOG.info( "Start main task" );
+        executorService = getNewThreadPool();
+        gtfsStaticData = new GTFSStaticData( gtfsPath );
+        gtfsManager = GTFSManager.createAndStart(
+                gtfsURL,
+                gtfsManagerTaskPeriod,
+                gtfsStaticData,
+                executorService );
+        busStopManager = new BusStopManager( gtfsManager );
 
-	private static void setUpTest( ) {
-		String busStopName = "Bryans / 108th Street";
-		Set<String> busStopIds = gtfsStaticData.getBusStopIDs( busStopName );
-		busStopManager.addBusStopToTrack(
-				busStopName );
-		busStopIds.forEach(
-				bsid -> executorService.scheduleAtFixedRate(
-						() -> busStopManager.getBusStop( bsid ).ifPresent(
-								bstop -> {
-										System.out.println(
-												busStopName );
-										System.out.println(
-												bstop.getBusStopId( ) );
-										System.out.println(
-												bstop.getUpcomingBus( 0 ).orElse( null ) );
-										System.out.println(
-												bstop.getUpcomingBus( 1 ).orElse( null ) );
-										System.out.println(
-												bstop.getUpcomingBus( 2 ).orElse( null ) );
-								} ),
-						0,
-						10, TimeUnit.SECONDS ) );
-	}
+        LOG.info( "Start initializing User IO" );
+        userIoManager = UserIOManager.createAndInit(
+                busStopManager,
+                numberOfTripsToShow,
+                userIoTaskPeriod,
+                executorService );
 
-	private static ScheduledThreadPoolExecutor getNewThreadPool( ) {
-		return new ScheduledThreadPoolExecutor( 5 );
-	}
+        LOG.info( "Finished init" );
+    }
+
+    private static ScheduledThreadPoolExecutor getNewThreadPool( ) {
+        return new ScheduledThreadPoolExecutor( 5 );
+    }
 }
